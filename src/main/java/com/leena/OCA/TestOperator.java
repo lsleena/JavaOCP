a340c163d78ae431c706dc8f58b2a1780726386e
lsleena <lsleena@gmail.com>
2019-03-13 16:55:34 -0700

    Added code for testing statements including switch statement

--- src/main/java/com/leena/OCA/TestOperator.java
+++ src/main/java/com/leena/OCA/TestOperator.java
@@ -11,12 +11,15 @@ public class TestOperator {
 
         System.out.println(x*y/z);
 
-     //Logical Complement and Negation Operators
+        System.out.println("*****************");
+        //Logical Complement and Negation Operators
 
         boolean x1 = false;
         System.out.println(x1);
         x1 = !x1;
         System.out.println(x1);
+
+        System.out.println("*****************");
 
         double x2 = 1.21;
         System.out.println(x2); // 1.21
@@ -24,5 +27,45 @@ public class TestOperator {
         System.out.println(x2); // -1.21
         x2 = -x2;
         System.out.println(x2); // 1.21
+
+        System.out.println("*****************");
+
+        int counter = 0;
+        System.out.println(counter);
+        System.out.println(++counter);
+        System.out.println(counter);
+        System.out.println(counter--);
+        System.out.println(counter);
+
+         x = 3;
+         y = ++x * 5 / x-- + --x;
+        System.out.println("x is " + x);
+        System.out.println("y is " + y);
+
+        System.out.println("********Casting Primitive Values*********");
+         x = (int)9.0;
+        System.out.println(x);
+        short y1 = (short)1921222;
+        System.out.println(y1);
+        System.out.print(2147483647+1); // -2147483648 overflow wraps to next negative value
+
+        System.out.println("********Inclusive / Exclusive OR*********");
+        int i = 1,j=9;
+        System.out.println((true^false) );
+        System.out.println(true|true);
+        System.out.println(true^true);// false
+
+
+        System.out.println("********Equality operator*********");
+
+        System.out.println("5 == 5.00 is  :"+(5 == 5.00));// returns true since the left side is promoted to a double
+
+        String str1 = new String("Test");
+        String str2 = new String("Test");
+
+        System.out.println(str1 + " == " + str2 + " - " + (str1 == str2));
+
+        System.out.println(str1 + ".equals(" + str2 + ") - " + str1.equals(str2));
+
     }
 }
--- src/main/java/com/leena/OCA/TestStatement.java
+++ src/main/java/com/leena/OCA/TestStatement.java
@@ -1,4 +1,117 @@
 package com.leena.OCA;
 
 public class TestStatement {
+
+    public void switchCaseDemo() {
+
+        System.out.println("**********Switch******");
+        int month = 7;
+
+        switch (month) {
+            case 1:
+                System.out.println("Month is January");
+                break;
+            case 2:
+                System.out.println("Month is February");
+                break;
+            case 3:
+                System.out.println("Month is March");
+                break;
+            case 4:
+                System.out.println("Month is April");
+                break;
+            case 5:
+                System.out.println("Month is May");
+                break;
+            case 6:
+                System.out.println("Month is June");
+                break;
+            case 7:
+                System.out.println("Month is July");
+
+            case 8:
+                System.out.println("Month is August");
+                break;
+            case 9:
+                System.out.println("Month is September");
+                break;
+            case 10:
+                System.out.println("Month is October");
+                break;
+            case 11:
+                System.out.println("Month is November");
+                break;
+            case 12:
+                System.out.println("Month is December");
+                break;
+        }
+    }
+
+    static void breakExample() {
+
+        System.out.println("********** Break Statement with lable ******");
+
+        int[][] list = {{1, 13, 5}, {1, 2, 5}, {2, 7, 2}};
+        int searchValue = 2;
+        int positionX = -1;
+        int positionY = -1;
+        PARENT_LOOP:
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list[i].length; j++) {
+                if (list[i][j] == searchValue) {
+                    positionX = i;
+                    positionY = j;
+                    break PARENT_LOOP;
+                }
+            }
+        }
+
+        if (positionX == -1 || positionY == -1) {
+            System.out.println("Value " + searchValue + " not found");
+        } else {
+            System.out.println("Value " + searchValue + " found at: " +
+                    "(" + positionX + "," + positionY + ")");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        TestStatement test = new TestStatement();
+
+        test.switchCaseDemo();
+
+        TestStatement.breakExample();
+
+
+        System.out.println("\n****************");
+
+        int x = 0;
+        long y = 10;
+        for (y = 0, x = 4; x < 8 && y < 10; x++, y++) {
+            System.out.print(x + " ");
+        }
+
+        System.out.println("\n**********Advanced Flow******");
+        x = 20;
+        while (x > 0) {
+            do {
+                x -= 2;
+            } while (x > 5);
+            x--;
+            System.out.print(x + "\t");
+        }
+
+
+        int[][] myComplexArray = {{5, 2, 1, 3}, {3, 9, 8, 9}, {5, 7, 12, 7}};
+        OUTER_LOOP:
+        for (int[] mySimpleArray : myComplexArray) {
+            INNER_LOOP:
+            for (int i = 0; i < mySimpleArray.length; i++) {
+                System.out.print(mySimpleArray[i] + "\t");
+            }
+            System.out.println();
+        }
+
+    }
+
 }
