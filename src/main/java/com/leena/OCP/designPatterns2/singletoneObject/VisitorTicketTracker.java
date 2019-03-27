1ea36f6a4a0b99ccfd2e50c281b17f7282624b4f
lsleena <lsleena@gmail.com>
2019-03-27 05:57:57 -0700

    Updated Singleton Object with double checked locking

--- src/main/java/com/leena/OCP/designPatterns2/singletoneObject/VisitorTicketTracker.java
+++ src/main/java/com/leena/OCP/designPatterns2/singletoneObject/VisitorTicketTracker.java
@@ -8,13 +8,29 @@ when the class is loaded but rather the first time it is requested by a client.
  */
 public class VisitorTicketTracker {
 
+/*
     private static VisitorTicketTracker instance;
 
-    public static VisitorTicketTracker getInstance() {
+    public static synchronized  VisitorTicketTracker getInstance() {
 
         if(instance == null) {
-            instance = new VisitorTicketTracker();
+            instance = new VisitorTicketTracker(); // not thread safe so putting synchronized so only one thread will be allowed in the method at a time
         }
         return  instance;
+    }
+*/
+
+// Singletons with Double‐Checked Locking
+
+    private static volatile VisitorTicketTracker instance;
+    public static VisitorTicketTracker getInstance() {
+        if(instance == null) {
+            synchronized(VisitorTicketTracker.class) {
+                if(instance == null) {
+                    instance = new VisitorTicketTracker();
+                }
+            }
+        }
+        return instance;
     }
 }
