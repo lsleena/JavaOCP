e5146a5433841529789de0361be3e51bc969c2f7
lsleena <lsleena@gmail.com>
2019-03-27 15:42:02 -0700

    Added Building Pattern

---
+++ src/main/java/com/leena/OCP/designPatterns2/builderPattern/AnimalBuilder.java
@@ -0,0 +1,49 @@
+package com.leena.OCP.designPatterns2.builderPattern;
+import com.leena.OCP.designPatterns2.immutableObjects.AnimalImmutable;
+
+import java.util.*;
+
+public class AnimalBuilder {
+
+    private String species;
+    private int age;
+    private List<String> favoriteFoods;
+
+    public AnimalBuilder setAge(int age) {
+
+        this.age = age;
+        return this;
+    }
+
+
+    public AnimalBuilder setSpecies(String species) {
+
+        this.species = species;
+        return this;
+    }
+
+    public AnimalBuilder setFavoriteFoods(List<String> favoriteFoods) {
+
+        this.favoriteFoods = favoriteFoods;
+        return this;
+    }
+
+    public AnimalImmutable build() {
+
+        return new AnimalImmutable(species, age, favoriteFoods);
+    }
+
+    public static void main(String[] args) {
+
+        AnimalBuilder duckBuilkder = new AnimalBuilder();
+        duckBuilkder
+                .setAge(4)
+                .setFavoriteFoods(Arrays.asList("meat","more meat")).setSpecies("duck");
+
+        AnimalImmutable duck = duckBuilkder.build();
+
+        AnimalImmutable flamingo = new AnimalBuilder().setFavoriteFoods(Arrays.asList("algea","insects"))
+                .setSpecies("flamingo").build();
+
+    }
+}
