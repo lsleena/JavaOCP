d08394420929d72b6199a4e23263d1d6c8138fad
lsleena <lsleena@gmail.com>
2019-04-09 11:48:05 -0700

    Updated with Runnable

--- src/main/java/com/leena/OCP/concurrency7/CalculateAverage.java
+++ src/main/java/com/leena/OCP/concurrency7/CalculateAverage.java
@@ -9,12 +9,30 @@ public class CalculateAverage implements Runnable {
 
         this.scores = scores;
     }
+
     @Override
     public void run () {
 
+        int  i = 0;
+        while (i <= 9) {
+
+            System.out.println(i++);
+        }
     }
 
     public static void main(String[] args) {
 
+        double[] num = {1.3,4.5};
+     //   new CalculateAverage(num).run();
+        (new Thread(new CalculateAverage(num))).start();
+        new Thread(new ReadInventoryThread()).start();
+    }
+}
+
+class ReadInventoryThread extends Thread {
+
+    public void run() {
+
+        System.out.println("Printing zoo inventory");
     }
 }
