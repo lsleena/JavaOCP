e280f90e016be069bae8f6ed9aa27b3d16e77785
lsleena <lsleena@gmail.com>
2019-04-10 14:09:20 -0700

    Updated with Runnable

--- src/main/java/com/leena/OCP/concurrency7/CalculateAverage.java
+++ src/main/java/com/leena/OCP/concurrency7/CalculateAverage.java
@@ -26,6 +26,12 @@ public class CalculateAverage implements Runnable {
      //   new CalculateAverage(num).run();
         (new Thread(new CalculateAverage(num))).start();
         new Thread(new ReadInventoryThread()).start();
+
+        System.out.println("begin");
+        (new ReadInventoryThread()).start();
+        (new Thread(new PrintData())).start();
+        (new ReadInventoryThread()).start();
+        System.out.println("end");
     }
 }
 
@@ -34,5 +40,19 @@ class ReadInventoryThread extends Thread {
     public void run() {
 
         System.out.println("Printing zoo inventory");
+    }
+}
+
+class PrintData implements Runnable {
+
+    public void run() {
+
+        for(int i=0; i<3; i++)
+            System.out.println("Printing record: "+i);
+    }
+
+    public static void main(String[] args) {
+
+        (new Thread(new PrintData())).start();
     }
 }
