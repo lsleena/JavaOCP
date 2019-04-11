fd2eacffa5f11391260ccd6ffa41028eba03bece
lsleena <lsleena@gmail.com>
2019-04-11 13:48:53 -0700

    Updated

--- src/main/java/com/leena/OCP/concurrency7/AddData.java
+++ src/main/java/com/leena/OCP/concurrency7/AddData.java
@@ -1,9 +1,6 @@
 package com.leena.OCP.concurrency7;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class AddData {
 
@@ -17,6 +14,8 @@ public class AddData {
 
             Future<Integer> result = service.submit(() -> 30+11);
 
+            // We can now retrieve and print the output of the Callable results, 41 in this example.
+
             System.out.println(result.get());
 
         } finally {
@@ -24,6 +23,31 @@ public class AddData {
             if (service != null) service.shutdown();
 
         }
+
+
+        if (service != null) {
+
+
+            if (service.isTerminated())
+                System.out.println("All tasks  finished");
+            else
+                System.out.println("Atleast one task is still running");
+
+
+            service.awaitTermination(1, TimeUnit.MINUTES);
+
+        }
+
+        if (service != null) {
+
+            service.awaitTermination(1, TimeUnit.MINUTES);
+
+            if (service.isTerminated())
+                System.out.println("Tasks are finished");
+            else
+                System.out.println("Atleast one task is still running");
+        }
+
 
     }
 }
