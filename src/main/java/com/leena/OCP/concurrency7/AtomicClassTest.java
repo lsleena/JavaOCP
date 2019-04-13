e84aed1321b9f98de6ca6ad9bbdb9ee673c3b677
lsleena <lsleena@gmail.com>
2019-04-13 15:08:03 -0700

    Updated AtomicClass

--- src/main/java/com/leena/OCP/concurrency7/AtomicClassTest.java
+++ src/main/java/com/leena/OCP/concurrency7/AtomicClassTest.java
@@ -13,9 +13,17 @@ public class AtomicClassTest {
     private int sheepCount = 0;
 
     private void incrementAndReport () {
+
         synchronized (this) {
-            System.out.println((++sheepCount) + " ");
+            System.out.println((++sheepCount) + " * ");
         }
+    }
+
+    private AtomicInteger sheepCount1 = new AtomicInteger(11);
+
+    private void incrementAndReport1() {
+
+        System.out.print(sheepCount1.incrementAndGet()+" ");
     }
 
     public static void main(String[] args) {
@@ -23,14 +31,26 @@ public class AtomicClassTest {
         ExecutorService service = null;
         try {
 
-            service = Executors.newFixedThreadPool(20);
+            service = Executors.newFixedThreadPool(10);
+
+            ExecutorService service1 = Executors.newFixedThreadPool(10);
+
 
             AtomicClassTest manager = new AtomicClassTest();
+
+            AtomicClassTest manager1 = new AtomicClassTest();
+
 
             for (int i=0; i<10; i++)
             {
                 synchronized (manager) {
                     service.submit(() -> manager.incrementAndReport());
+                }
+            }
+            for (int j=10; j<20; j++)
+            {
+                synchronized (manager1) {
+                    service1.submit(() -> manager1.incrementAndReport1());
                 }
             }
         }
@@ -42,3 +62,5 @@ public class AtomicClassTest {
 
 
 }
+
+
