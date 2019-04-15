328078d8199da4bb17b8ce1af4831cad413d18f0
lsleena <lsleena@gmail.com>
2019-04-15 14:39:59 -0700

    Updated the code for Cyclic Barrier

--- src/main/java/com/leena/OCP/concurrency7/CyclicBarrierTest.java
+++ src/main/java/com/leena/OCP/concurrency7/CyclicBarrierTest.java
@@ -1,6 +1,8 @@
 package com.leena.OCP.concurrency7;
 
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -28,7 +30,38 @@ class LionPenManager {
 
 public class CyclicBarrierTest {
 
+    private void removeAnimals() {
+        System.out.println("Removing animals");
+    }
+
+    private void cleanPen() {
+        System.out.println("Cleaning the pen");
+    }
+
+    private void addAnimals() {
+        System.out.println("Adding animals");
+    }
+
+    public void performTask(CyclicBarrier c1, CyclicBarrier c2) {
+
+        try {
+
+            removeAnimals();
+            c1.await();
+            cleanPen();
+            c2.await();
+            addAnimals();
+
+        } catch (InterruptedException | BrokenBarrierException e) {
+
+        }
+    }
+
     public static void main(String[] args) {
+
+        System.out.println("\nWithout Using CyclicBarrier Class:\n");
+
+        System.out.println("***********************************");
 
         ExecutorService service = null;
         try {
@@ -37,12 +70,36 @@ public class CyclicBarrierTest {
 
             LionPenManager manager = new LionPenManager();
 
-            for (int i =0;i<4; i++)
+            for (int i = 0; i<4; i++)
                 service.submit(() -> manager.performtask());
+
         } finally {
 
             if (service != null) service.shutdown();
         }
 
+        System.out.println("\nUsing CyclicBarrier Class:\n");
+
+        System.out.println("***********************************");
+
+        try {
+
+            service = Executors.newFixedThreadPool(4);
+
+            CyclicBarrierTest manager = new CyclicBarrierTest();
+
+            CyclicBarrier c1 = new CyclicBarrier(4);
+
+            CyclicBarrier c2 = new CyclicBarrier(4,
+                    () -> System.out.println("****** Pen Cleaned ! "));
+
+
+            for (int i = 0; i<4; i++)
+                service.submit(() -> manager.performTask(c1, c2));
+        }
+        finally {
+
+            if (service != null) service.shutdown();
+        }
     }
 }
