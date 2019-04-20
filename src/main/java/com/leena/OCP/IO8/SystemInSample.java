9e804a6379219001f46a11f4aa4be10b49fbac01
lsleena <lsleena@gmail.com>
2019-04-20 07:12:36 -0700

    Added SystemIn Sample

---
+++ src/main/java/com/leena/OCP/IO8/SystemInSample.java
@@ -0,0 +1,16 @@
+package com.leena.OCP.IO8;
+
+import java.io.*;
+
+
+public class SystemInSample {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        String userInput = reader.readLine();
+        System.out.println("You entered the following: "+userInput);
+    }
+}
