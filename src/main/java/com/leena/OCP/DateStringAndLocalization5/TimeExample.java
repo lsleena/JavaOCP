0dca9f59c3c97f441aaf0fb73315e69c72609ee5
lsleena <lsleena@gmail.com>
2019-04-24 14:56:12 -0700

    Added Time Example

---
+++ src/main/java/com/leena/OCP/DateStringAndLocalization5/TimeExample.java
@@ -0,0 +1,15 @@
+package com.leena.OCP.DateStringAndLocalization5;
+
+
+import java.time.*;
+
+public class TimeExample {
+
+    public static void main(String[] args) {
+
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+        System.out.println(LocalDateTime.now());
+        System.out.println(ZonedDateTime.now());
+    }
+}
