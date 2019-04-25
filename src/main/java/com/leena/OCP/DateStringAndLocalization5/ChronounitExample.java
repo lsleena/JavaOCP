d4989233288dc827f3ea41f06e29104f3f3ca8ea
lsleena <lsleena@gmail.com>
2019-04-25 10:54:48 -0700

    Updated chronounit example

--- src/main/java/com/leena/OCP/DateStringAndLocalization5/ChronounitExample.java
+++ src/main/java/com/leena/OCP/DateStringAndLocalization5/ChronounitExample.java
@@ -10,8 +10,41 @@ public class ChronounitExample {
         LocalTime one = LocalTime.of(5, 15);
         LocalTime two = LocalTime.of(6, 30);
         LocalDate date = LocalDate.of(2016, 1, 20);
+
+        LocalDate date1 = LocalDate.of(2017, 1, 20);
+
         System.out.println(ChronoUnit.HOURS.between(one, two)); // 1
         System.out.println(ChronoUnit.MINUTES.between(one, two)); // 75
-        System.out.println(ChronoUnit.MINUTES.between(one, date)); // DateTimeException
+
+        System.out.println(ChronoUnit.DAYS.between(date, date1)); // DateTimeException
+
+      //  System.out.println(ChronoUnit.MINUTES.between(one, date)); // DateTimeException
+
+
+        LocalDate date2 = LocalDate.of(2013,1,20);
+
+        LocalTime time = LocalTime.of(23,4);
+
+        LocalDateTime dateTime = LocalDateTime.of(date2, time);
+
+        Duration duration = Duration.ofHours(2);
+
+        System.out.println(dateTime);
+
+        System.out.println(dateTime.plus(duration));
+
+     //   System.out.println(date2.plus(duration));
+
+        System.out.println(time.plus(duration));
+
+        LocalDate date3 = LocalDate.of(2015, 5, 25);
+        Period period = Period.ofDays(1);
+        Duration days = Duration.ofDays(1);
+        System.out.println(date3.plus(period)); // 2015–05–26
+
+        System.out.println(days.toString()); // Unsupported unit: Seconds
+
+    System.out.println(date3.plus(days)); // Unsupported unit: Seconds
+
     }
 }
