e156122e7d4af17538583038a15cf0c965b7970f
lsleena <lsleena@gmail.com>
2019-04-25 08:35:42 -0700

    Added period example

---
+++ src/main/java/com/leena/OCP/DateStringAndLocalization5/PeriodExample.java
@@ -0,0 +1,34 @@
+package com.leena.OCP.DateStringAndLocalization5;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.Period;
+
+public class PeriodExample {
+    private static void performAnimalEnrichment(LocalDate start, LocalDate end,
+                                                Period period1) { // uses the generic period
+        LocalDate upTo = start;
+        while (upTo.isBefore(end)) {
+            System.out.println("give new toy: " + upTo);
+            upTo = upTo.plus(period1); // adds the period
+        }
+    }
+
+    public static void main(String[] args) {
+
+        LocalDate localDate = LocalDate.of(2018, Month.APRIL, 30);
+
+        LocalDate localDate1 = LocalDate.of(2019, 04, 9);
+
+        Period period = Period.ofMonths(3);
+
+        System.out.println(period);
+        LocalDate start = LocalDate.of(2015, Month.JANUARY, 1);
+        LocalDate end = LocalDate.of(2015, Month.MARCH, 30);
+        Period period1 = Period.ofMonths(1); // create a period
+        performAnimalEnrichment(start, end, period1);
+    }
+
+
+}
