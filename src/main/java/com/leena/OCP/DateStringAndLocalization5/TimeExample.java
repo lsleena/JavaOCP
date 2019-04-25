c0e64de478f600c43f1b48c51482466697739858
lsleena <lsleena@gmail.com>
2019-04-25 07:14:37 -0700

    Updated

--- src/main/java/com/leena/OCP/DateStringAndLocalization5/TimeExample.java
+++ src/main/java/com/leena/OCP/DateStringAndLocalization5/TimeExample.java
@@ -34,5 +34,16 @@ public class TimeExample {
         System.out.println(LocalTime.now());
         System.out.println(LocalDateTime.now());
         System.out.println(ZonedDateTime.now());
+
+         LocalDate date = LocalDate.of(2014, Month.JANUARY, 20);
+         System.out.println(date); // 2014–01–20
+        date = date.plusDays(2);
+        System.out.println(date); // 2014–01–22
+        date = date.plusWeeks(1);
+        System.out.println(date); // 2014–01–29
+        date = date.plusMonths(1);
+       System.out.println(date); // 2014–02–28
+        date = date.plusYears(5);
+      System.out.println(date); // 2019–02–28
     }
 }
