fe02c547f78e79aa04dcb7e4836896f492e9972a
lsleena <lsleena@gmail.com>
2019-04-28 08:06:10 -0700

    Added resource bundle

---
+++ src/main/java/com/leena/OCP/DateStringAndLocalization5/ZooOpen.java
@@ -0,0 +1,22 @@
+package com.leena.OCP.DateStringAndLocalization5;
+
+import java.time.*;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class ZooOpen {
+
+    public static void main(String[] args) {
+        Locale us = new Locale("en", "US");
+        Locale france = new Locale("fr", "FR");
+        printProperties(us);
+        System.out.println();
+         printProperties(france);
+        }
+
+       public static void printProperties(Locale locale) {
+        ResourceBundle rb = ResourceBundle.getBundle("Zoo", locale);
+      System.out.println(rb.getString("hello"));
+       System.out.println(rb.getString("open"));
+    }
+}
