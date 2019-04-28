8bb9cdbf4a619f65432be2d372897454eb5d9fe4
lsleena <lsleena@gmail.com>
2019-04-28 09:14:11 -0700

    Updated resource bundle

---
+++ src/main/java/com/leena/OCP/DateStringAndLocalization5/Zoo_en.java
@@ -0,0 +1,17 @@
+package com.leena.OCP.DateStringAndLocalization5;
+
+import java.util.ListResourceBundle;
+
+public class Zoo_en extends ListResourceBundle {
+
+    @Override
+    protected Object[][] getContents() {
+        return new Object[][]{
+
+                {"hello", "Hello"},
+                {"open", "The zoo is open"}
+        };
+    }
+}
+
+
