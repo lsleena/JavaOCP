e96b6cfe73ee08228fad4b6dd031d1e7e940dfc8
lsleena <lsleena@gmail.com>
2019-04-29 06:22:29 -0700

    Added format numbers

---
+++ src/main/java/com/leena/OCP/DateStringAndLocalization5/FOrmatNumbers.java
@@ -0,0 +1,18 @@
+package com.leena.OCP.DateStringAndLocalization5;
+
+import java.text.*;
+import java.util.*;
+
+public class FOrmatNumbers {
+    public static void main(String[] args) {
+
+        int attendeesPerYear = 3_200_000;
+        int attendeesPerMonth = attendeesPerYear / 12;
+        NumberFormat us = NumberFormat.getInstance(Locale.US);
+        System.out.println(us.format(attendeesPerMonth));
+        NumberFormat g = NumberFormat.getInstance(Locale.GERMANY);
+        System.out.println(g.format(attendeesPerMonth));
+        NumberFormat ca = NumberFormat.getInstance(Locale.CANADA_FRENCH);
+        System.out.println(ca.format(attendeesPerMonth));
+    }
+}
