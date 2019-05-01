2ef5a81bb4134d15374543515f9f4d689b2bddd5
lsleena <lsleena@gmail.com>
2019-05-01 16:12:15 -0700

    Added mutlicatch exception example

---
+++ src/main/java/com/leena/OCP/ExceptionAndAssertion6/MultiCatchException.java
@@ -0,0 +1,31 @@
+package com.leena.OCP.ExceptionAndAssertion6;
+
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class MultiCatchException {
+
+    public static void main(String[] args) {
+
+        try {
+
+            Path path = Paths.get("dolphinsBorn.txt");
+            String text = new String(Files.readAllBytes(path));
+            LocalDate date = LocalDate.parse(text);
+            System.out.println(date);
+
+        } catch (DateTimeParseException e) {
+
+            e.printStackTrace();
+            throw new RuntimeException(e);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+}
