d9f7a590e76b7adbd6835f555d7140701e59a259
lsleena <lsleena@gmail.com>
2019-05-01 16:01:48 -0700

    Added custom exception example

---
+++ src/main/java/com/leena/OCP/ExceptionAndAssertion6/CannotSwimException.java
@@ -0,0 +1,19 @@
+package com.leena.OCP.ExceptionAndAssertion6;
+
+public class CannotSwimException {
+
+    public CannotSwimException() {
+        super();
+    }
+    public CannotSwimException(Exception e) {
+        super();
+    }
+  /*  public CannotSwimException(String message) {
+        super(message);
+    }*/
+
+  public static void main(String[] args){
+
+
+  }
+}
