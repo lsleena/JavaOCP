1df3be52f82bb93fdc037b75b50bec9f597bfc74
lsleena <lsleena@gmail.com>
2019-05-02 05:17:59 -0700

    Updated mutlicatch exception example

--- src/main/java/com/leena/OCP/ExceptionAndAssertion6/MultiCatchException.java
+++ src/main/java/com/leena/OCP/ExceptionAndAssertion6/MultiCatchException.java
@@ -8,7 +8,7 @@ import java.time.format.DateTimeParseException;
 
 public class MultiCatchException {
 
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
 
         try {
 
@@ -27,5 +27,17 @@ public class MultiCatchException {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
-    }
+    }*/
+
+    public static void main(String[] args) {
+        try {
+            Path path = Paths.get("dolphinsBorn.txt");
+            String text = new String(Files.readAllBytes(path));
+            LocalDate date = LocalDate.parse(text);
+            System.out.println(date);
+        } catch (Exception e) { // BAD approach
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } }
+
 }
