b6d27b3dca9dfbc00b3d9676699bd87291a5aafa
lsleena <lsleena@gmail.com>
2019-05-03 13:31:18 -0700

    Added assertion

---
+++ src/main/java/com/leena/OCP/ExceptionAndAssertion6/Assertion.java
@@ -0,0 +1,34 @@
+package com.leena.OCP.ExceptionAndAssertion6;
+
+import static com.leena.OCP.ExceptionAndAssertion6.Seasons.SUMMER;
+
+ enum Seasons {
+    SPRING, SUMMER, FALL
+}
+public class Assertion {
+
+
+    public static void test(Seasons s) {
+        switch (s) {
+            case SPRING:
+            case FALL:
+                System.out.println("Shorter hours");
+                break;
+            case SUMMER:
+                System.out.println("Longer hours");
+                break;
+            default:
+                assert false : "Invalid season";
+        }
+    }
+
+    public static void main(String[] args) {
+
+      /*  int num = -5;
+        assert num > 0;
+        System.out.println(num);*/
+
+      Assertion.test(WINTER);
+
+    }
+}
