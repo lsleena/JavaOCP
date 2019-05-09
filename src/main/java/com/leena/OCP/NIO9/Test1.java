bc697b1c6191fdbbc24d73d40c537d4d04b192b5
lsleena <lsleena@gmail.com>
2019-05-09 08:14:19 -0700

    Added path methods

---
+++ src/main/java/com/leena/OCP/NIO9/Test1.java
@@ -0,0 +1,21 @@
+package com.leena.OCP.NIO9;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        try (InputStream is = new FileInputStream("/Users/leenapatil/workspace/JavaOCP/src/main/resources/test1.txt");
+             OutputStream out = new FileOutputStream("/Users/leenapatil/workspace/JavaOCP/src/main/resources/test2.txt")) {
+// Copy stream data to file
+            Files.copy(is, Paths.get("/Users/leenapatil/workspace/JavaOCP/src/main/resources/test2.txt"));
+// Copy file data to stream
+            Files.copy(Paths.get("/Users/leenapatil/workspace/JavaOCP/src/main/resources/test1.txt"), out);
+        } catch (IOException e) {
+// Handle file I/O exception...
+        }
+    }
+}
