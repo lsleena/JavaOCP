23aaab6bdf02227d37f4706e1dc5292687854df6
lsleena <lsleena@gmail.com>
2019-05-09 10:49:45 -0700

    Added BasicFileAttributesSample

---
+++ src/main/java/com/leena/OCP/NIO9/BasicFileAttributesSample.java
@@ -0,0 +1,27 @@
+package com.leena.OCP.NIO9;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+
+public class BasicFileAttributesSample {
+
+    public static void main(String[] args) throws IOException {
+
+        Path path = Paths.get("/Users/leenapatil/workspace/JavaOCP/src/main/resources/data/testChild.txt");
+        BasicFileAttributes data = Files.readAttributes(path,
+                BasicFileAttributes.class);
+        System.out.println("Is path a directory? "+data.isDirectory());
+        System.out.println("Is path a regular file? "+data.isRegularFile());
+        System.out.println("Is path a symbolic link? "+data.isSymbolicLink());
+        System.out.println("Path not a file, directory, nor symbolic link? "+
+                data.isOther());
+        System.out.println("Size (in bytes): "+data.size());
+        System.out.println("Creation date/time: "+data.creationTime());
+        System.out.println("Last modified date/time: "+data.lastModifiedTime());
+        System.out.println("Last accessed date/time: "+data.lastAccessTime());
+        System.out.println("Unique file identifier (if available): "+
+                data.fileKey());
+    }
+}
