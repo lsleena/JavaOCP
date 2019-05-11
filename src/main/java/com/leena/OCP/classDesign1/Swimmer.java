c28df2012b0e8b5b27eae5671dfeee595e87c5d9
lsleena <lsleena@gmail.com>
2019-05-11 16:52:52 -0700

    Added Enum Example

---
+++ src/main/java/com/leena/OCP/classDesign1/Swimmer.java
@@ -0,0 +1,19 @@
+package com.leena.OCP.classDesign1;
+
+public class Swimmer {
+
+    enum AnimalClasses {
+        MAMMAL, FISH {
+            public boolean hasFins() { return true; }},
+        BIRD, REPTILE, AMPHIBIAN, INVERTEBRATE;
+        public abstract boolean hasFins();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(AnimalClasses.FISH);
+        System.out.println(AnimalClasses.FISH.ordinal());
+        System.out.println(AnimalClasses.FISH.hasFins());
+        System.out.println(AnimalClasses.BIRD.hasFins());
+    }
+}
