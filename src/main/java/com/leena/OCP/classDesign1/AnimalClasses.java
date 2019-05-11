e0700764a7cca859100383635bfe1a8f6b133f07
lsleena <lsleena@gmail.com>
2019-05-11 16:52:52 -0700

    Added Enum Example

---
+++ src/main/java/com/leena/OCP/classDesign1/AnimalClasses.java
@@ -0,0 +1,12 @@
+package com.leena.OCP.classDesign1;
+
+public enum AnimalClasses {
+
+    MAMMAL(true), FISH(Boolean.FALSE), BIRD(false);
+    boolean hasHair;
+
+    public AnimalClasses(boolean hasHair) {
+         this.hasHair = hasHair;
+         }
+
+}
