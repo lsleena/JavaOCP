7d6d018007f2934c2ee7c8147455d6d94ec39180
lsleena <lsleena@gmail.com>
2019-05-11 16:44:35 -0700

    Added test Example

---
+++ src/main/java/com/leena/OCP/classDesign1/Tets.java
@@ -0,0 +1,16 @@
+package com.leena.OCP.classDesign1;
+
+public class Tets {
+
+    public static void main(String[] args) {
+
+        char c = 'A';
+
+        System.out.println( calc(c));
+
+    }
+    public static int calc(int n) {
+
+        return n;
+    }
+    }
