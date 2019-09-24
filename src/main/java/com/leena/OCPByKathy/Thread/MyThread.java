93003ef2a78e3d6ea2ba78ee50167a656ed1783b
lsleena <lsleena@gmail.com>
2019-09-24 09:18:35 -0700

    Runnable and Threads

--- src/main/java/com/leena/OCPByKathy/Thread/MyThread.java
+++ src/main/java/com/leena/OCPByKathy/Thread/MyThread.java
@@ -1,4 +1,9 @@
 package com.leena.OCPByKathy.Thread;
 
-public class MyThread {
+public class MyThread extends Thread{
+
+    public void run() {
+
+        System.out.println("Running important job of MyThread");
+    }
 }
